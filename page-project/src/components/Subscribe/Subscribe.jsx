a8dc5185ae541a2b30c3a84beb6450eb43a5da32
OksanaNan <oksana.semka@gmail.com>
2023-07-20 16:06:39 +0300

    Fixed subscribe component

--- page-project/src/components/Subscribe/Subscribe.jsx
+++ page-project/src/components/Subscribe/Subscribe.jsx
@@ -2,11 +2,13 @@ import styles from './subscribe.module.scss';
 
 export const Subscribe = () => {
     return (
-        <section className={`${styles.Subscribe}p3`}>
-         <div className={`${styles.SubscribeContainer} paddings container flexCenter`}>
+        <section className={`${styles.Subscribe} paddings`}>
+         <div className={`${styles.SubscribeContainer}  container flexCenter`}>
            <div className={`${styles.SubscribeContainerDesc}`}>
-              <h2 className={`title`}>Get The Best Updates</h2>
-              <p>Subscribe with your mail id to know the current update in book <br/> edition at your fingertip with in a minute.</p>
+            <div className={`${styles.SubscribeContainerDescTitle}`}>
+              <h2 className={`title cardo`}>Get The Best Updates</h2>
+              <p className={`inter`}>Subscribe with your mail id to know the current update in book <br/> edition at your fingertip with in a minute.</p>
+            </div>
             <form className={`${styles.SubscribeContainerDescForm}`}>
                 <input type="email" placeholder="Your Email ID..." />
                 <button className="subscribe">Subscribe</button>
